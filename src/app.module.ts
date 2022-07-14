a9b6ad04863e19e984ace5081ff828b67dad4c0d
takamuu <mr.takamuu@gmail.com>
2022-07-14 21:36:04 +0900

    Migrate DB connection settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [
-    ItemsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
-      autoLoadEntities: true,
-    }),
-  ],
+  imports: [ItemsModule, TypeOrmModule.forRoot()],
   controllers: [],
   providers: [],
 })
