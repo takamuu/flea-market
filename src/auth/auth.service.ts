f9240c3f7c7645a1188579ab4fc0d489eda67a10
takamuu <mr.takamuu@gmail.com>
2022-07-15 13:49:31 +0900

    Create controllers and services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userRepository: UserRepository) {}
+
+  async signUp(createUserDto: CreateUserDto): Promise<User> {
+    return await this.userRepository.createUser(createUserDto);
+  }
+}
